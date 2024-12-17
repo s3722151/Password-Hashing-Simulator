c161689ed9d76120d48343ab29f1176cc817ce15
Zetazone <37312796+Zetazone@users.noreply.github.com>
2024-12-17 20:51:15 +1100

    Made some comments on references to look on how to hasj.

--- src/main.java
+++ src/main.java
@@ -20,3 +20,19 @@ public class main {
 //INSERT METHODS HERE
 
 }
+//REFERENCES
+/* 
+Simple Hash
+https://www.w3schools.com/java/tryjava.asp?filename=demo_ref_string_hashcode
+
+What is bytes
+https://www.youtube.com/watch?v=Dnd28lQHquU
+	What is the number system: https://www.youtube.com/watch?v=FFDMzbrEXaE
+
+What is SHA-256
+https://medium.com/@AlexanderObregon/what-is-sha-256-hashing-in-java-0d46dfb83888
+
+How to hash
+https://www.geeksforgeeks.org/sha-256-hash-in-java/
+
+ */
